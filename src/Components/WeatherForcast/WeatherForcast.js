a048eee83cc1b984116e09ffe7a85f3c0e9957d0
Abrish-seng <abrish814@gmail.com>
2025-08-13 14:27:54 +0300

    adding the forcast edited file

--- src/Components/WeatherForcast/WeatherForcast.js
+++ src/Components/WeatherForcast/WeatherForcast.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import { format, addDays } from 'date-fns';
+import { format} from 'date-fns';
 import './WeatherForcast.css';
 
 const WeatherForecast = ({ data }) => {
